575594f8ef4be66e4d94cd601772dcda39fddf46
PujithaSaiD <147297735+PujithaSaiD@users.noreply.github.com>
2023-12-29 16:43:35 +0530

    Add files via upload

---
+++ Numberpattern.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Numberpattern {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        List<Integer> l=new ArrayList<Integer>();
+        for(int i=0;i<n;i++){
+            l.add(sc.nextInt());
+        }
+        for(int j=0;j<l.size();j++){
+            for(int k=1;k<=l.get(j);k++){
+                for(int m=1;m<=k;m++){
+                    System.out.print(m);
+                }
+                System.out.println();
+            }
+        }
+        
+    }
+    
+}
